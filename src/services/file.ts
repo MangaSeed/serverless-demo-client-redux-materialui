3137ef78b372e0af1b5185ee8f665fda3df29d28
Vincent <vincent.nocum@gmail.com>
2019-11-13 13:15:05 +0800

    fix(type): added correct type for storage vault response

--- src/services/file.ts
+++ src/services/file.ts
@@ -1,16 +1,20 @@
-import { Storage } from 'aws-amplify'
+import { Storage } from 'aws-amplify';
+
+interface IVaultPutResponse {
+  key: string;
+}
 
 export const fileUpload = async (file: File) => {
-  const name = `${Date.now()}-${file.name}`
-  return await Storage.vault.put(name, file, {
+  const name = `${Date.now()}-${file.name}`;
+  return (await Storage.vault.put(name, file, {
     contentType: file.type
-  })
-}
+  })) as IVaultPutResponse;
+};
 
 export const fileGet = async (key: string) => {
-  return await Storage.vault.get(key)
-}
+  return await Storage.vault.get(key);
+};
 
 export const fileDelete = async (key: string) => {
-  return await Storage.vault.remove(key)
-}
+  return await Storage.vault.remove(key);
+};
