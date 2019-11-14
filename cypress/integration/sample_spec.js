9bf2d13212562d3d8d803a9090c2253b3fa0a04f
xuatz <xzlow10@gmail.com>
2019-11-14 23:34:30 +0900

    asljdhasjdhsadsad

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,16 @@
-describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true);
+describe('Landing Page', () => {
+  beforeEach(() => {
+    cy.visit('localhost:3000');
+  });
+  it('should show "A simple note taking app"', () => {
+    cy.findByText('A simple note taking app').should('have.length', 1);
+  });
+
+  describe('When clicking "LOG IN"', () => {
+    it('we should see the login form', () => {
+      cy.findByText('LOG IN').click();
+      cy.findByLabelText('Email').should('have.length', 1);
+      cy.findByLabelText('Password').should('have.length', 1);
+    });
   });
 });
