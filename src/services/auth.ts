10467e3028eca534e9e45d0123d9338221935d87
Vincent <vincent.nocum@gmail.com>
2019-11-14 21:17:51 +0800

    refact: convert auth service arrow funtions -> normal

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,28 +1,27 @@
 import { Auth } from 'aws-amplify';
 import { CognitoUser } from '@aws-amplify/auth';
-import { CognitoUserSession } from 'amazon-cognito-identity-js';
 
-export const signIn = async (
+export async function signIn(
   email: string,
   password: string
-): Promise<CognitoUser> => {
+): Promise<CognitoUser> {
   return await Auth.signIn(email, password);
-};
+}
 
-export const check = async (): Promise<CognitoUserSession> => {
+export async function check() {
   return await Auth.currentSession();
-};
+}
 
-export const signOut = async () => {
+export async function signOut() {
   return await Auth.signOut();
-};
+}
 
-export const signUp = async (email: string, password: string) => {
+export async function signUp(email: string, password: string) {
   return await Auth.signUp({
     password,
     username: email
   });
-};
+}
 
 export const activate = async (email: string, code: string) => {
   return await Auth.confirmSignUp(email, code);
