cf812e15fea71cd85b57dc5083a35b240c10a4b3
Vincent <vincent.nocum@gmail.com>
2019-11-14 21:18:27 +0800

    feat(auth): registered auth reducer to root

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
 import { noteReducer } from './note';
+import { authReducer } from './auth';
 
 export const rootReducer = combineReducers({
+  auth: authReducer,
   note: noteReducer
 });
 
