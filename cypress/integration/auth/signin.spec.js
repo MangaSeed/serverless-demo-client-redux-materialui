bb2c5795b2b2250e97f5d5f74ef12182ad28b3c0
Vincent <vincent.nocum@gmail.com>
2019-11-19 23:44:16 +0800

    test: added test for auth signin, signup, signout

---
+++ cypress/integration/auth/signin.spec.js
@@ -0,0 +1,60 @@
+// THIS COULD BE AN ENV OR CONFIG FILE
+const MAIL = 'admin@example.com';
+const PASS = 'Passw0rd!';
+
+const WRONG_MAIL = 'asfasfas@example.com';
+const WRONG_PASS = 'asfajshf123!';
+
+const COGNITO = 'https://cognito-identity.us-east-1.amazonaws.com/';
+
+describe('Authentication - Sign in', () => {
+  beforeEach(() => {
+    cy.visit('/signin');
+  });
+
+  it('should have a signin page', () => {
+    cy.get('#email').should('have.length', 1);
+    cy.get('#password').should('have.length', 1);
+    cy.get('#signin-button').should('have.length', 1);
+  });
+
+  it('should have a disabled button when no email given', () => {
+    cy.get('#password').type(WRONG_PASS);
+    cy.get('#signin-button').should('be.disabled');
+  });
+
+  it('should have a disabled button when no password given', () => {
+    cy.get('#email').type(MAIL);
+    cy.get('#signin-button').should('be.disabled');
+  });
+
+  it('should have a disabled button when no password and mail given', () => {
+    cy.get('#password').type(WRONG_PASS);
+    cy.get('#email').type(MAIL);
+
+    cy.get('#password').clear();
+    cy.get('#email').clear();
+
+    cy.get('#signin-button').should('be.disabled');
+  });
+
+  it('should error and alert error w/ wrong credentials', () => {
+    cy.get('#email').type(WRONG_MAIL);
+    cy.get('#password').type(WRONG_PASS);
+    cy.get('#signin-button').click();
+    cy.on('window:alert', str => {
+      expect(str).to.equal('User does not exist.');
+    });
+  });
+
+  it('should be able to sign in w/ correct credentials', () => {
+    cy.server();
+    cy.route('POST', COGNITO).as('signIn');
+    cy.get('#email').type(MAIL);
+    cy.get('#password').type(PASS);
+    cy.get('#signin-button').click();
+    cy.wait('@signIn')
+      .its('status')
+      .should('be', 200);
+  });
+});
--- cypress/integration/sample.spec.js
+++ cypress/integration/sample.spec.js
@@ -2,6 +2,7 @@ describe('Landing Page', () => {
   beforeEach(() => {
     cy.visit('localhost:3000');
   });
+
   it('should show "A simple note taking app"', () => {
     cy.findByText('A simple note taking app').should('have.length', 1);
   });
