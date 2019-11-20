349fca1cad953bbbe810e402b38b327eb6775f3d
Vincent <vincent.nocum@gmail.com>
2019-11-20 16:23:02 +0800

    feat(selector): removing of note

--- src/store/selector/note.ts
+++ src/store/selector/note.ts
@@ -25,3 +25,20 @@ export const selectNoteCreateData = createSelector(
   selectNoteCreate,
   create => create.data
 );
+
+export const selectNoteRemove = createSelector(selectNote, note => note.remove);
+
+export const selectNoteRemoving = createSelector(
+  selectNoteRemove,
+  remove => remove.removing
+);
+
+export const selectNoteRemoved = createSelector(
+  selectNoteRemove,
+  remove => remove.removed
+);
+
+export const selectNoteRemoveError = createSelector(
+  selectNoteRemove,
+  remove => remove.error
+);
