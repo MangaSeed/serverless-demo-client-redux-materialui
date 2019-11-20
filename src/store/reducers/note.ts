9333663d6b7021b466ca65cbd1f7a87326b0a190
Vincent <vincent.nocum@gmail.com>
2019-11-20 19:49:52 +0800

    feat(reducer): note update
    Actions, actions types, payload interface

--- src/store/reducers/note.ts
+++ src/store/reducers/note.ts
@@ -23,9 +23,17 @@ interface INoteRemoveState {
   error: string;
 }
 
+interface INoteUpdateState {
+  updating: boolean;
+  updated: boolean;
+  error: string;
+  data: INote | null;
+}
+
 interface INoteState {
   create: INoteCreateState;
   remove: INoteRemoveState;
+  update: INoteUpdateState;
 }
 
 export interface ICreateNotePayload {
@@ -38,6 +46,11 @@ export interface IRemoveNotePayload {
   fileName?: string;
 }
 
+export interface IUpdateNotePayload {
+  note: INote;
+  attachment?: File | null;
+}
+
 interface ICreateNoteAction {
   payload: ICreateNotePayload;
 }
@@ -46,14 +59,20 @@ interface IRemoveNoteAction {
   payload: IRemoveNotePayload;
 }
 
+interface IUpdateNoteAction {
+  payload: IUpdateNotePayload;
+}
+
 /** NOTE ACTION TYPES */
 
 type CreateNoteActionType = (params: ICreateNotePayload) => ICreateNoteAction;
 type RemoveNoteActionType = (params: IRemoveNotePayload) => IRemoveNoteAction;
+type UpdateNoteActionType = (params: IUpdateNotePayload) => IUpdateNoteAction;
 
 /** NOTE CONSTANTS */
 export const CREATE_NOTE = 'note/createNoteAction';
 export const REMOVE_NOTE = 'note/removeNoteAction';
+export const UPDATE_NOTE = 'note/updateNoteAction';
 
 const INIT_NOTE_CREATE_STATE: INoteCreateState = {
   creating: false,
@@ -68,9 +87,17 @@ const INIT_NOTE_REMOVE_STATE: INoteRemoveState = {
   error: '',
 };
 
+const INIT_NOTE_UPDATE_STATE: INoteUpdateState = {
+  updating: false,
+  updated: false,
+  error: '',
+  data: null,
+};
+
 const INIT_NOTE_STATE: INoteState = {
   create: INIT_NOTE_CREATE_STATE,
   remove: INIT_NOTE_REMOVE_STATE,
+  update: INIT_NOTE_UPDATE_STATE,
 };
 
 /** NOTE SLICE CONFIG */
@@ -109,9 +136,25 @@ const noteSlice = createSlice({
       state.remove = { ...INIT_NOTE_REMOVE_STATE, error: payload };
     },
 
+    updatingNoteAction: state => {
+      state.update = { ...INIT_NOTE_UPDATE_STATE, updating: true };
+    },
+
+    updatedNoteAction: (state, { payload }: PayloadAction<INote>) => {
+      state.update = {
+        ...INIT_NOTE_UPDATE_STATE,
+        updated: true,
+        data: payload,
+      };
+    },
+
+    updateNoteErrorAction: (state, { payload }: PayloadAction<string>) => {
+      state.update = { ...INIT_NOTE_UPDATE_STATE, error: payload };
+    },
+
     clearNoteStateAction: (
       state,
-      { payload }: PayloadAction<'create' | 'remove'>
+      { payload }: PayloadAction<'create' | 'remove' | 'update'>
     ) => {
       (state[payload] as typeof state[typeof payload]) = INIT_NOTE_STATE[
         payload
@@ -131,14 +174,22 @@ export const removeNoteAction = createAction<
   typeof REMOVE_NOTE
 >(REMOVE_NOTE, params => ({ payload: params }));
 
+export const updateNoteAction = createAction<
+  UpdateNoteActionType,
+  typeof UPDATE_NOTE
+>(UPDATE_NOTE, params => ({ payload: params }));
+
 export const {
   clearNoteStateAction,
-  creatingNoteAction,
   createdNoteAction,
   createNoteErrorAction,
+  creatingNoteAction,
   removedNoteAction,
   removeNoteErrorAction,
   removingNoteAction,
+  updatedNoteAction,
+  updateNoteErrorAction,
+  updatingNoteAction,
 } = noteSlice.actions;
 
 /** NOTE REDUCER */
